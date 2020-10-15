4dfcf8bacb66f16974e94bca4b5153e2eddc4215
Adam-Rapoport <adam.rapoport@chicagobooth.edu>
2020-10-15 21:33:18 +0000

    1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
 
+  def blank_add_form
+render({ :template => "calculation_templates/add_form.html.erb" })
+end
+
+def blank_subtract_form
+render({ :template => "calculation_templates/subtract_form.html.erb" })
+end
+
+def blank_multiply_form
+render({ :template => "calculation_templates/multiply_form.html.erb" })
+end
+
+def blank_divide_form
+render({ :template => "calculation_templates/divide_form.html.erb" })
+end
+
 end
