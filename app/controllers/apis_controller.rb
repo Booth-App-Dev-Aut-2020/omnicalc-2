5a74d63e98a2b04733f525a29caba55e42ad3ba9
Adam-Rapoport <adam.rapoport@chicagobooth.edu>
2020-10-22 20:03:37 +0000

    3

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -5,12 +5,13 @@ render({ :template => "weather_templates/coord_form.html.erb" })
 end
 
 def address_coords
-address = params.fetch("user_address")
+@address = params.fetch("user_address")
 key = GMAP_KEY
-url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=" + key
+url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + @address + "&key=" + key
 raw_data=open(url).read
 parsed_data=JSON.parse(raw_data)
-
+@longitude = parsed_data.dig("results", 0, "geometry", "location", "lng")
+@latitude = parsed_data.dig("results", 0, "geometry", "location", "lat")
 
 render({ :template => "weather_templates/address_coords.html.erb" })
 end
