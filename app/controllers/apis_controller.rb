b25caf4b860e7218809563727edf79407f9992cb
Adam-Rapoport <adam.rapoport@chicagobooth.edu>
2020-10-22 19:53:37 +0000

    2

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -6,7 +6,8 @@ end
 
 def address_coords
 address = params.fetch("user_address")
-url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDB6uZXmYRlo88RLhAxD-yxUbMIZd4oHpg"
+key = GMAP_KEY
+url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=" + key
 raw_data=open(url).read
 parsed_data=JSON.parse(raw_data)
 
